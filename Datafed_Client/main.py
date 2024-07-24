126325b493165bd6198bae344aa06e1a23a5e23f
jagar2 <jca92@drexel.edu>
2024-07-24 15:59:17 -0400

    push

--- Datafed_Client/main.py
+++ Datafed_Client/main.py
@@ -1,11 +1,16 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget
 
+# Only needed for access to command line arguments
+import sys
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
 app = QApplication(sys.argv)
+
+# Create a Qt widget, which will be our window.
 window = QWidget()
-window.setWindowTitle('PyQt6 App')
-window.setGeometry(100, 100, 280, 80)
-helloMsg = QLabel('<h1>Hello, Docker with PyQt6!</h1>', parent=window)
-helloMsg.move(60, 15)
-window.show()
-sys.exit(app.exec())
+window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+app.exec()
